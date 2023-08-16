aa98587c5c007ac342a15bf397999babd8b14134
nkwook <ruddnrdl@kaist.ac.kr>
2023-08-16 10:18:46 +0000

    fixed install_carla script

--- install_carla.sh
+++ install_carla.sh
@@ -1,11 +1,11 @@
 cd ~/Documents/self-driving-cars
-curl -O <https://carla-releases.s3.eu-west-3.amazonaws.com/Linux/CARLA_0.9.11_RSS.tar.gz>
+curl -O https://carla-releases.s3.eu-west-3.amazonaws.com/Linux/CARLA_0.9.11_RSS.tar.gz
 mkdir carla_0911_rss
 tar -xvzf CARLA_0.9.11_RSS.tar.gz -C carla_0911_rss
 cd ~/Documents/self-driving-cars/carla_0911_rss/PythonAPI/carla/dist
 sudo apt-get install python-setuptools
 sudo python -m easy_install carla-0.9.11-py3.7-linux-x86_64.egg
 cd ~/Documents/self-driving-cars/carla_0911_rss
-curl -O <https://carla-releases.s3.eu-west-3.amazonaws.com/Linux/AdditionalMaps_0.9.11.tar.gz>
+curl -O https://carla-releases.s3.eu-west-3.amazonaws.com/Linux/AdditionalMaps_0.9.11.tar.gz
 mv AdditionalMaps_0.9.11.tar.gz Import/
 ./ImportAssets.sh
